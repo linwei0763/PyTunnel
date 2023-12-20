28ddaa915e4bedfbb1ab0ec5eee67b895d0c03d5
Wei Lin <102598371+linwei0763@users.noreply.github.com>
2023-12-20 10:09:48 +0000

    Update generate_synthetic_data.py

--- generate_synthetic_data.py
+++ generate_synthetic_data.py
@@ -1,5 +1,6 @@
 import copy
 import numpy as np
+import os
 import random as rd
 
 
@@ -126,6 +127,7 @@ def rotate_xy(pc, ang):
 if __name__ == '__main__':
     
     '''config'''
+    path_o = 'result'
     fns = ['1-1', '1-2', '1-3', '1-4', '1-5', '1-6', '1-7', '1-8', '1-9', '1-10', '1-11', '1-12', '1-13', '1-14', '1-15', '1-16', '1-17', '2-1', '2-2', '2-3', '2-4', '2-5', '2-6', '2-7', '2-8', '2-9', '2-10', '2-11', '2-12', '2-13', '2-14']
     
     len_seg = 1.5
@@ -139,7 +141,7 @@ if __name__ == '__main__':
     '''call'''
     for fn in fns:
         pc = generate_tunnel(len_seg, r, wid_joi, dep_joi, res, res_joi)
-        np.savetxt('result/' + fn + '.txt', pc, fmt='%.8f %.8f %.8f %.8f %d')
+        np.savetxt(os.path.join(path_o, fn + '.txt'), pc, fmt='%.8f %.8f %.8f %.8f %d')
     '''call'''
     
     
