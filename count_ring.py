73a54c2cccbab02ccf3f132c2eca3d2f4aefba51
Wei Lin <102598371+linwei0763@users.noreply.github.com>
2023-12-25 21:25:53 +0100

    Create count_ring.py

---
+++ count_ring.py
@@ -0,0 +1,29 @@
+import numpy as np
+import os
+import pandas as pd
+
+if __name__ == '__main__':
+    
+    '''------config------'''
+    path_i = '../Seg2Tunnel/seg2tunnel'
+    
+    tunnel_no = ['1', '2']
+    '''------config------'''
+    
+    files = os.listdir(path_i)
+    
+    
+    rings = {}
+    
+    for tunnel in tunnel_no:
+        rings[tunnel] = []
+    
+    for file in files:
+        tunnel = file.split('-')[0]
+        ring = file.split('.')[0].split('-')[-1]
+        if tunnel in rings.keys():
+            rings[tunnel].append(ring)
+            
+            
+    
+    
